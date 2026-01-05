6b5577297823f32eeadfcf8f9bef0a49d738fbc1
Developer <developer@example.com>
2026-01-06 06:44:37 +0800

    feat: Upgrade Next.js to v16.1.1 and enable Turbopack.

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
   images: {
     formats: ['image/webp'],
   },
+  turbopack: {},
   webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
     // Important: return the modified config
     config.module.rules.push({
